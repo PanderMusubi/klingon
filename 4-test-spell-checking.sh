e114a70278ec0277c84b4bf9ef2be0fe74048a81
PanderMusubi <pander@users.sourceforge.net>
2018-05-12 21:36:33 +0200

    fixed typo in filename

--- 4-test-spell-checking.sh
+++ 4-test-spell-checking.sh
@@ -3,9 +3,9 @@ if [ ! -e tlh_Latn.aff -a ! -e tlh_Latn.dic -a ! tests.txt ]; then
 	exit 1
 fi
 echo 'INFO: Testing '`wc -l klingon-latin | awk '{print $1}'`' words'
-hunspell -d tlh_Latn -a klingon-latin | grep '^#' > words_incorrext.txt
-echo 'INFO: '`wc -l words_incorrext.txt | awk '{print $1}'`' words are incorrect'
+hunspell -d tlh_Latn -a klingon-latin | grep '^#' > words_incorrect.txt
+echo 'INFO: '`wc -l words_incorrect.txt | awk '{print $1}'`' words are incorrect'
 echo 'INFO: Testing '`wc -l tests | awk '{print $1}'`' words'
-hunspell -d tlh_Latn -a tests | grep '^#' > tests_incorrext.txt
-echo 'INFO: '`wc -l tests_incorrext.txt | awk '{print $1}'`' words are incorrect'
+hunspell -d tlh_Latn -a tests | grep '^#' > tests_incorrect.txt
+echo 'INFO: '`wc -l tests_incorrect.txt | awk '{print $1}'`' words are incorrect'
 #FIXME let apostrophe be recognized as a word character
