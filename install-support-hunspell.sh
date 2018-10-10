70c13f5fe3bf71ea0e0e94a947114eb47b9b2304
PanderMusubi <pander@users.sourceforge.net>
2018-10-10 17:57:47 +0200

    transfer

--- install-support-hunspell.sh
+++
@@ -1,4 +0,0 @@
-if [ ! -e /usr/share/hunspell/ ]; then
-	mkdir -p /usr/share/hunspell/
-fi
-sudo cp -f tlh_Latn.??? /usr/share/hunspell/
--- install-word-list.sh
+++
@@ -1,5 +0,0 @@
-if [ ! -e /usr/share/dict/ ]; then
-	mkdir -p /usr/share/dict/
-fi
-sudo cp -f klingon-latin /usr/share/dict/
-sudo ln -sf /usr/share/dict/klingon-latin /usr/share/dict/tlhingan-latin
